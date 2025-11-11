88c082169f37958ecac6ac940f92c80c4ee7c547
CaoimhinCondon-Design <caoimhin@edgewheres.com>
2025-11-11 10:52:55 +0100

    Got rid of this as it is not a library

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { OpenAI } from "openai";
 import { PrismaClient } from "@prisma/client/extension";
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 import crypto from "crypto";
 
 dotenv.config();
